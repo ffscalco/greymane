0eb38f68cac868e37a81e39130239d5e611a2e22
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2016-05-31 19:52:14 -0300

    Removed bourbon and added material-admin style.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,20 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery2
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
+//= require js/libs/jquery-ui/jquery-ui.min
+//= require js/libs/bootstrap/bootstrap.min
+
+//= require js/core/source/App
+//= require js/core/source/AppCard
+//= require js/core/source/AppForm
+//= require js/core/source/AppNavigation
+//= require js/core/source/AppVendor
+
+//= require_self
+
+Turbolinks.enableProgressBar();
