1876d93f63759117768d35d2b256e122cd45c912
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2016-05-31 22:26:31 -0300

    Moved google app id and key to env variables.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,7 +243,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, "1061086811693-24o29q3dtq8vag2bhelgn9d7med2pm9o.apps.googleusercontent.com", "OQGRz7MvFFsUMY_E3emRusfm", { access_type: "offline", approval_prompt: "" }
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], { access_type: "offline", approval_prompt: "" }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
