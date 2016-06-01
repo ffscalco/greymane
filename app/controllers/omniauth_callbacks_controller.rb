4c17d725544dc7c7f2c0c5f64beb6431a6dcbfe7
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2016-05-31 23:27:54 -0300

    Refactored flash messages and redirect path in case of failure.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,11 +3,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      flash[:success] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      flash[:error] = I18n.t "devise.omniauth_callbacks.failure", :kind => "Google"
+      redirect_to root_path
     end
   end
 end
