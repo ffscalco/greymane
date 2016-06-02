f403d8e834dddf0862595a0f4f760bd2ed2131fd
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2016-06-02 00:10:29 -0300

    Refactored find_for_google_oauth2 method to create an user with team and image too.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,27 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :trackable, :omniauthable, :omniauth_providers => [:google_oauth2]
 
+  belongs_to :team
+  validates :team, presence: true
+
   def self.find_for_google_oauth2(access_token)
     data = access_token.info
     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
-    return user if user
+    return user unless user.nil?
 
-    registered_user = User.where(:email => access_token.info.email).first
-    return registered_user if registered_user
+    registered_user = User.where(:email => data["email"]).first
+    return registered_user unless registered_user.nil?
 
-    user = User.create(
+    team = Team.get_team_by_email(data["email"])
+
+    User.create(
       name: data["name"],
-      provider:access_token.provider,
+      provider: access_token.provider,
       email: data["email"],
       uid: access_token.uid ,
-      password: Devise.friendly_token[0,20]
+      image: data["image"],
+      password: Devise.friendly_token[0,20],
+      team_id: team.id
     )
   end
 end
