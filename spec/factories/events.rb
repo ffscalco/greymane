8a683d86d1d846324e6abf94d8b5e0b89e6887a8
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2016-06-28 17:38:40 -0300

    Corrected format for event.price.

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :event do
     name "MyString"
     local "MyString"
-    price 9.99
+    price "9,99"
     site "MyString"
     subject "MyText"
     start_date "2016-06-02"
